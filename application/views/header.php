54bfc607327e0ae7c7a4eee7bcca4bbf5c97c2fb
vandii <vengeful.vandana@gmail.com>
2016-04-06 14:15:31 +0530

    added bootstrap

--- application/views/header.php
+++ application/views/header.php
@@ -17,7 +17,9 @@
 	<!--[if lt IE 9]>
 		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+
 	<![endif]-->
+<link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.css"); ?>" />
 </head>
 <body>
 
@@ -46,6 +48,10 @@
 			</div><!-- .container-fluid -->
 		</nav><!-- .navbar -->
 	</header><!-- #site-header -->
+	<script type="text/javascript" src="<?php echo base_url("assets/js/jQuery-1.10.2.js"); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.js"); ?>"></script>
+
+</body>
 
 	<main id="site-content" role="main">
 		
